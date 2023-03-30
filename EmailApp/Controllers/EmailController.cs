60936caf51d93e49d9a091ade893198fccbfaf2a
HenryBri <99488927+HenryBri@users.noreply.github.com>
2023-03-30 18:53:08 +0300

    first

---
+++ EmailApp/Controllers/EmailController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit.Text;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace EmailApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService _emailService;
+        public EmailController(IEmailService emailService) 
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailDto request)
+        {
+            _emailService.SendEmail(request);
+
+            return Ok();
+        }
+    }
+}
